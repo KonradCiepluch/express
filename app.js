c86592ac47ca5b5568fab0cd2a13faa3cf12a8d3
Konrad Ciepluch <konrad190008@wp.pl>
2021-11-14 17:31:31 +0100

    Admin login, admin news,

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var createError = require("http-errors");
+var cookieSession = require("cookie-session");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var config = require("./config");
+var mongoose = require("mongoose");
+mongoose.connect(config.db, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
 
 var indexRouter = require("./routes/index");
 var newsRouter = require("./routes/news");
@@ -20,7 +27,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(
+  cookieSession({
+    name: "session",
+    keys: config.keySession,
+    maxAge: config.maxAgeSession,
+  })
+);
 app.use(function (req, res, next) {
   console.log(req.path);
   res.locals.path = req.path;
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: "mongodb+srv://admin:wljAHgIvJQ2X45o2@cluster0.er0ny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  keySession: ["TWOJKLUCZ"],
+  maxAgeSession: 24 * 60 * 60 * 1000,
+};
